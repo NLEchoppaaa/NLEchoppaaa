7d2c36345612323262d45f68139c30d5e01300bc
NLEchoppaaa <accskibidi@gmail.com>
2024-10-07 00:07:52 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Aboutus from "@/components/Aboutus";
+import BannerUI from "@/components/BannerUI";
+import CrispApp from "@/components/Crisp";
+import Faq from "@/components/Faq";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Products from "@/components/Products";
+import TopNotification from "@/components/TopNotification";
+import Background from "@/components/Background"
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <CrispApp />
+      <TopNotification />
+      <Header />
+      <Hero />
+      <Aboutus />
+      <Products />
+      <Features />
+      <Faq />
+      <BannerUI />
+      <Footer />
+      <Background />
+    </>
+  );
+}
+
+export const dynamic = "force-dynamic";
